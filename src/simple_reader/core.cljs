55afc232577848a69f6133d70435e1cb68c09646
William <William@undefined.re>
2017-04-14 09:13:51 +0200

    if cli wants unread only, but unread = 0, show read instead

--- src/simple_reader/core.cljs
+++ src/simple_reader/core.cljs
@@ -59,7 +59,12 @@
                    articles (io/read-feed f-dir)
                    ;view-values   ["unread" "saved" "all"]
                    articles (condp = view 
-                              "unread"  (filter #(not= "read" (-> % :metadata :status)) articles)
+                              "unread"  (let [unread (filter #(not= "read" (-> % :metadata :status)) articles)
+                                              unsaved (filter #(not= "saved" (-> % :metadata :status)) unread)]
+                                          (println :unread unsaved)
+                                          (if (= 0 (count unsaved))
+                                            articles
+                                            unread))
                               "saved"   (filter #(= "saved" (-> % :metadata :status)) articles)
                               articles)
                    articles (condp = order
