3a22fe16f1e73d1d0284ee0e7995010f7f42a11c
William <William@undefined.re>
2018-03-04 18:24:25 +0100

    add favicon

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -396,8 +396,7 @@
 
 (init)
 
-(defn on-js-reload []
+(defn on-js-reload [])
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  )
